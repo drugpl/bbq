e6f8a5bb4f86dbe46306645d9ac80ac03e9f921f
Paweł Pacana <pawel.pacana@gmail.com>
2011-05-07 13:03:10 +0200

    added vendored rails app fo testing

---
+++ bbq/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'd3e800a2ca370da2d7b2e46784c6eeb39daf13e4762b7171d4fa2171e3e2f057c0f29d2db4a20047f0a8a842305309b22628fc73e6372b6d2bfe08e0818f1b2e'
