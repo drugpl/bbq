cf20adac7bf0686a7b4cf704288e2380da792cd7
Peter Suschlik <peter-gavatar@suschlik.de>
2011-05-29 02:13:19 -0700

    Check for class. #to_s not needed

--- bbq/lib/bbq/util.rb
+++ bbq/lib/bbq/util.rb
@@ -3,12 +3,12 @@ require 'active_support/core_ext/string/inflections'
 module Bbq
   class Util
     def self.find_module(name, scope = nil)
-      namespace = case scope.class.name
-      when "String", "Symbol"
+      namespace = case scope.class
+      when String, Symbol
         "::#{scope.to_s.camelize}"
-      when "Class"
+      when Class
         "::#{scope.name}"
-      when "NilClass"
+      when NilClass
         nil
       else
         "::#{scope.class.name}"
