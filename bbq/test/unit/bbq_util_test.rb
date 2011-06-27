4ac6384d4afce3b092f2240db8c3a4c9e3b4ef1d
Robert Pankowecki <robert.pankowecki@gmail.com>
2011-06-27 23:23:56 +0200

    refactored bbq_util_test

--- bbq/test/unit/bbq_util_test.rb
+++ bbq/test/unit/bbq_util_test.rb
@@ -11,32 +11,29 @@ end
 
 class BbqUtilTest < Test::Unit::TestCase
   
-  def test_find_module_in_object
-    user = User.new
-    [:commenter, "commenter"].each do |name|
-      obj  = Bbq::Util.find_module(name, user)
-      assert_equal obj, User::Commenter
-    end
+  def test_find_module_in_object_namespace
+    assert_commenter(User.new, User::Commenter)
   end
 
-  def test_find_global_module
-    [:commenter, "commenter"].each do |name|
-      obj  = Bbq::Util.find_module(name)
-      assert_equal obj, ::Commenter
-    end
-  end
-
-  def test_find_module_in_class
-    [:commenter, "commenter"].each do |name|
-      obj  = Bbq::Util.find_module(name, User)
-      assert_equal obj, User::Commenter
-    end
+  def test_find_module_in_class_namespace
+    assert_commenter(User, User::Commenter)
   end
 
   def test_find_module_in_string_namespace
+    assert_commenter("User", User::Commenter)
+  end
+
+  def test_find_global_module
+    assert_commenter(nil, ::Commenter)
+  end
+
+  
+  private
+
+
+  def assert_commenter(namespace, result)
     [:commenter, "commenter"].each do |name|
-      obj  = Bbq::Util.find_module(name, "User")
-      assert_equal obj, User::Commenter
+      assert_equal Bbq::Util.find_module(name, namespace), result
     end
   end
 
