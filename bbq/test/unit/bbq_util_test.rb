6d351433584bf6031e83c3e1ae0bb91178a251ef
Robert Pankowecki <robert.pankowecki@gmail.com>
2011-06-27 23:19:44 +0200

    Added more testcases for module finder

--- bbq/test/unit/bbq_util_test.rb
+++ bbq/test/unit/bbq_util_test.rb
@@ -10,11 +10,34 @@ module Commenter
 end
 
 class BbqUtilTest < Test::Unit::TestCase
-  def test_find_module
+  
+  def test_find_module_in_object
     user = User.new
     [:commenter, "commenter"].each do |name|
       obj  = Bbq::Util.find_module(name, user)
       assert_equal obj, User::Commenter
     end
   end
+
+  def test_find_global_module
+    [:commenter, "commenter"].each do |name|
+      obj  = Bbq::Util.find_module(name)
+      assert_equal obj, ::Commenter
+    end
+  end
+
+  def test_find_module_in_class
+    [:commenter, "commenter"].each do |name|
+      obj  = Bbq::Util.find_module(name, User)
+      assert_equal obj, User::Commenter
+    end
+  end
+
+  def test_find_module_in_string_namespace
+    [:commenter, "commenter"].each do |name|
+      obj  = Bbq::Util.find_module(name, "User")
+      assert_equal obj, User::Commenter
+    end
+  end
+
 end
