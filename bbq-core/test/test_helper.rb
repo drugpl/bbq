1579a2b486b6976337dcb371ad18304e858e57e4
Paweł Pacana <pawel.pacana@gmail.com>
2011-06-28 02:43:34 +0200

    implicit schema loading for tests

--- bbq-core/test/test_helper.rb
+++ bbq-core/test/test_helper.rb
@@ -6,5 +6,11 @@ require "rails/test_help"
 
 Rails.backtrace_cleaner.remove_silencers!
 
+# Load database schema
+ActiveRecord::Base.configurations = YAML.load(File.read(File.expand_path("../dummy/config/database.yml", __FILE__)))
+ActiveRecord::Base.establish_connection("test")
+ActiveRecord::Migration.verbose = false
+load(File.expand_path("../dummy/db/schema.rb", __FILE__))
+
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
