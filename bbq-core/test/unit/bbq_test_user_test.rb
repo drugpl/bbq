aa5ed0a2dd63ad6e96177185582a46543d8396a7
Paweł Pacana <pawel.pacana@gmail.com>
2011-06-28 03:50:21 +0200

    explicit/implicit test user "eyes"

--- bbq-core/test/unit/bbq_test_user_test.rb
+++ bbq-core/test/unit/bbq_test_user_test.rb
@@ -21,14 +21,14 @@ end
 class BbqTestUserTest < Test::Unit::TestCase
 
   def test_capybara_dsl_methods
-    user = TestUser.new()
+    user = TestUser.new
     Capybara::Session::DSL_METHODS.each do |m|
       assert user.respond_to?(m)
     end
   end
 
   def test_roles
-    user = TestUser.new()
+    user = TestUser.new
     %w(comment upload moderate).each { |m| assert !user.respond_to?(m) }
 
     user.roles(:commenter, "comment_moderator")
@@ -39,11 +39,19 @@ class BbqTestUserTest < Test::Unit::TestCase
     %w(comment upload moderate).each { |m| assert user.respond_to?(m) }
   end
 
-  def test_user_eyes
-    @user = TestUser.new()
+  def test_implicit_user_eyes
+    @user = TestUser.new
     @user.register
-    assert_raises(MiniTest::Assertion){ @user.not_see?("BBQ") }
-    assert_raises(MiniTest::Assertion){ @user.see?("MIRACLE") }
+    assert_raises(MiniTest::Assertion){ @user.not_see!("BBQ") }
+    assert_raises(MiniTest::Assertion){ @user.see!("MIRACLE") }
+    User.find_by_email(@user.email).destroy
+  end
+
+  def test_explicit_user_eyes
+    @user = TestUser.new
+    @user.register
+    assert @user.see?("BBQ")
+    assert @user.not_see?("MIRACLE")
     User.find_by_email(@user.email).destroy
   end
 
