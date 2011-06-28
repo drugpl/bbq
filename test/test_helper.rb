867a6c29064e6e9b67a72aa549f54cd8f8874ad2
Paweł Pacana <pawel.pacana@gmail.com>
2011-06-28 03:37:23 +0200

    hack for tests passing on ruby 1.8.x

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,12 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
+if RUBY_VERSION < "1.9"
+  gem "minitest"
+  `use_minitest yes`
+  at_exit { `use_minitest no` }
+end
+
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
 
