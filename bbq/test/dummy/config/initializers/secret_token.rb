00e93bf1478d9df84dabfb3821d3ac2c28784b1c
Paweł Pacana <pawel.pacana@gmail.com>
2011-06-28 02:45:30 +0200

    regenerated vendored app:
      - 3.0.x Rails app (was 3.1.x before)
      - removed devise views, unused helpers and migrations

--- bbq/test/dummy/config/initializers/secret_token.rb
+++ bbq/test/dummy/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'd3e800a2ca370da2d7b2e46784c6eeb39daf13e4762b7171d4fa2171e3e2f057c0f29d2db4a20047f0a8a842305309b22628fc73e6372b6d2bfe08e0818f1b2e'
+Dummy::Application.config.secret_token = '04a135bb493e34c7dd6b2d0cd336d28a5ea8a5e30d2cc228573aadaaba4646636729f70f9381a0dc1ecc1be8fdff208d2be3b9b7fc5788f4f0d0466778e4bd10'
