845f9cc5164c4f5e1e8b065292d800a631d80d72
Paweł Pacana <pawel.pacana@gmail.com>
2011-06-29 13:32:38 +0200

    reworked generators with tests

--- bbq/lib/generators/bbq/install_generator.rb
+++ bbq/lib/generators/bbq/install_generator.rb
@@ -1,20 +1,5 @@
-require 'bbq/generator'
-
 module Bbq
   class InstallGenerator < Rails::Generators::Base
-    include Bbq::Generator
-
-    def create_directory
-      empty_directory test_root
-    end
-
-    def generate_test_user
-      empty_directory support_root
-      template "test_user.rb", "#{support_root}/test_user.rb"
-    end
-
-    def show_readme
-      readme "README.#{test_framework_short}" if behavior == :invoke
-    end
+    hook_for :test_framework, :aliases => "-t", :as => "bbq_install"
   end
 end
