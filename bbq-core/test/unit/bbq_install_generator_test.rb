bcabdd7d1ec49df1abef3a7bd33f1598da6c9f59
Paweł Pacana <pawel.pacana@gmail.com>
2011-06-30 00:28:32 +0200

    generators: change support directory to more common one

--- bbq-core/test/unit/bbq_install_generator_test.rb
+++ bbq-core/test/unit/bbq_install_generator_test.rb
@@ -11,13 +11,13 @@ class BbqInstallGeneratorTest < Rails::Generators::TestCase
     run_generator %w(-t test_unit)
 
     assert_directory "test/acceptance"
-    assert_directory "test/acceptance/support"
+    assert_directory "test/support"
   end
 
   def test_creating_test_unit_test_user_stub
     run_generator %w(-t test_unit)
 
-    assert_file "test/acceptance/support/test_user.rb", /class TestUser < Bbq::TestUser/
-    assert_file "test/acceptance/support/test_user.rb", /require "test_helper"/
+    assert_file "test/support/test_user.rb", /class TestUser < Bbq::TestUser/
+    assert_file "test/support/test_user.rb", /require "test_helper"/
   end
 end
