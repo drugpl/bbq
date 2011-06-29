aa50c02b75c31948f707aba08ef6163a025523e0
Paweł Pacana <pawel.pacana@gmail.com>
2011-06-29 23:46:09 +0200

    refactor test which should not depend on devise helpers

--- bbq-core/test/dummy/app/controllers/application_controller.rb
+++ bbq-core/test/dummy/app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_filter :authenticate_user!
   protect_from_forgery
 end
--- bbq-core/test/dummy/app/controllers/home_controller.rb
+++ bbq-core/test/dummy/app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
+  before_filter :authenticate_user!, :only => :index
+
   def index
     render :text => "BBQ"
+  end
+
+  def miracle
+    render :text => "MIRACLE"
   end
 end
