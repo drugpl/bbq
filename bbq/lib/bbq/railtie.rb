6ac2f6ef9eccdb3f071e82a506cb141c91cb8210
Paweł Pacana <pawel.pacana@gmail.com>
2011-06-30 14:39:58 +0200

    hide some indirect generators from being listed in rails

--- bbq/lib/bbq/railtie.rb
+++ bbq/lib/bbq/railtie.rb
@@ -1,7 +1,15 @@
+require 'rails/generators'
+
 module Bbq
   class Railtie < Rails::Railtie
     rake_tasks do
       load Bbq.root.join("lib/tasks/bbq.rake")
     end
+
+    helper_generators = %w(test_unit rspec).collect do |test_framework|
+      ["#{test_framework}:bbq_test", "#{test_framework}:bbq_install"]
+    end.flatten
+
+    Rails::Generators.hide_namespaces *helper_generators
   end
 end
