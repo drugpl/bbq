7994d57a1402181869287ecc83995deb2e025dfd
Paweł Pacana <pawel.pacana@gmail.com>
2011-06-30 22:10:51 +0200

    fixed implicit_user_eyes test:
      - removed from test_user_test where common implementation is placed
      - tested now in test_unit/rspec flavours as implementations differ

--- bbq/test/unit/bbq_test_unit_test.rb
+++ bbq/test/unit/bbq_test_unit_test.rb
@@ -28,4 +28,28 @@ class BbqTestUnitTest < Test::Unit::TestCase
     run_cmd 'ruby -Ilib -Itest/dummy/test test/dummy/test/acceptance/dsl_test.rb'
     assert_match /1 tests, 1 assertions, 0 failures, 0 errors/, output
   end
+
+  def test_implicit_user_eyes
+    create_file 'test/dummy/test/acceptance/implicit_user_eyes_test.rb', <<-TESTUNIT
+      require 'test_helper'
+      require 'bbq/test'
+
+      FAILED_ASSERTION = RUBY_VERSION < "1.9" ? Test::Unit::AssertionFailedError : MiniTest::Assertion
+
+      class ImplicitUserEyesTest < Bbq::TestCase
+        scenario 'should see welcome text' do
+          user = Bbq::TestUser.new
+          user.visit "/miracle"
+          user.see!("MIRACLE")
+          user.not_see!("BBQ")
+
+          assert_raises(FAILED_ASSERTION) { @user.see!("BBQ") }
+          assert_raises(FAILED_ASSERTION) { @user.not_see!("MIRACLE") }
+        end
+      end
+    TESTUNIT
+
+    run_cmd 'ruby -Ilib -Itest/dummy/test test/dummy/test/acceptance/dsl_test.rb'
+    assert_match /1 tests, 1 assertions, 0 failures, 0 errors/, output
+  end
 end
