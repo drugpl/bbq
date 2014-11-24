1c645df7071023fa87a6251d5f65a928f142298a
Paweł Pacana <pawel.pacana@gmail.com>
2014-11-24 01:06:45 +0100

    Another required change for new Rails.

--- bbq-core/test/dummy/config/routes.rb
+++ bbq-core/test/dummy/config/routes.rb
@@ -2,8 +2,8 @@ Dummy::Application.routes.draw do
   devise_for :users
   root :to => "home#index"
 
-  match "/miracle" => "home#miracle"
-  match "/ponycorns" => "home#ponycorns"
-  match "/rainbow" => "home#rainbow"
-  match "/uh_oh" => "home#uh_oh"
+  get "/miracle" => "home#miracle"
+  get "/ponycorns" => "home#ponycorns"
+  get "/rainbow" => "home#rainbow"
+  get "/uh_oh" => "home#uh_oh"
 end
